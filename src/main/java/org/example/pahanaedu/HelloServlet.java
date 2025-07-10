2a8272c6edd710c0d580817251aa04d1b32be46a
DilshanMP <mpdilshan.99@gmail.com>
2025-07-10 23:19:47 +0530

    add project files to git hub

---
+++ src/main/java/org/example/pahanaedu/HelloServlet.java
@@ -0,0 +1,19 @@
+package org.example.pahanaedu;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+
+
+    public void destroy() {
+    }
+}
