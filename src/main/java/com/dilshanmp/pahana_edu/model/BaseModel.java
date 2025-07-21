966eaed48b28aacdb757357f1328924fd0a9f83e
DilshanMp <mpdilshan.99@gmail.com>
2025-07-21 16:57:35 +0530

    DBConnection edit and Base model update+

---
+++ src/main/java/com/dilshanmp/pahana_edu/model/BaseModel.java
@@ -0,0 +1,45 @@
+package com.dilshanmp.pahana_edu.model;
+
+
+import java.sql.Timestamp;
+
+public abstract class BaseModel{
+    protected int id;
+    protected Timestamp createdAt;
+    protected Timestamp updateAt;
+
+   public BaseModel(){
+       this.createdAt = new Timestamp(System.currentTimeMillis());
+       this.updateAt = new Timestamp(System.currentTimeMillis());
+   }
+
+
+   //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Timestamp updateAt) {
+        this.updateAt = updateAt;
+    }
+
+
+
+
+}
